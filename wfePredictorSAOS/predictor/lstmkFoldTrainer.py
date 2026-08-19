a218302273202feb5e2cc54199ef6c45e644961b
nrodlin <nicolas.linares@iac.es>
2026-08-19 09:33:38 +0100

    Corrected path

--- wfePredictorSAOS/predictor/lstmkFoldTrainer.py
+++ wfePredictorSAOS/predictor/lstmkFoldTrainer.py
@@ -226,7 +226,7 @@ if __name__ == "__main__":
     batch_size = 64
     pred_horizon = 2
 
-    dalia_path = '/net/dalia/scratch/nlinares/results/results/predictor/training'
+    dalia_path = '/net/dalia/scratch/nlinares/results/results/predictor/training/training_val'
     local_path = '/home/nlinares/simulations/results/predictorSAOS/training/training_val'
 
     if os.path.exists(dalia_path):
