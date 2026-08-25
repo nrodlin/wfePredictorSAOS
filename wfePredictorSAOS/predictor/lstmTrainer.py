9cb8eb25539a5b0ce9d0f118ccbedfd17637b67e
Nicolas Rodriguez Linares <nlinares@durum.ll.iac.es>
2026-08-25 19:31:18 +0100

    Batch adjusted to fit in memory

--- wfePredictorSAOS/predictor/lstmTrainer.py
+++ wfePredictorSAOS/predictor/lstmTrainer.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
     learning_rate = 3e-4
     weight_decay = 1e-4
     max_epochs = 400
-    batch_size = 128
+    batch_size = 64
     pred_horizon = 2
     model_save_path = "best_model_IndepLSTM.pt"
 
